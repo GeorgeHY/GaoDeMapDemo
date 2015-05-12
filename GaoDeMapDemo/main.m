f0ea610820eed69d3767b15a289aead5df981dc2
GeorgeHY <hanyangkid@163.com>
2015-05-13 05:30:26 +0800

    Initial Commit

---
+++ GaoDeMapDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GaoDeMapDemo
+//
+//  Created by 韩扬 on 15/5/13.
+//  Copyright (c) 2015年 iwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
